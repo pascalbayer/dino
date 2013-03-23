95f96491603bb946cda5e122b6a7a07e966c3bee
Christopher Cliff <ctcliff@gmail.com>
2013-03-23 17:26:19 -0400

    Fixed bug in object serializer.

--- test/types/object.js
+++ test/types/object.js
@@ -14,4 +14,13 @@ describe('Object', function(){
             should.not.exist(type.parseValue('a'));
         });
     });
+    describe('transformValue', function(){
+        it('transforms the value', function(){
+            should.not.exist(type.transformValue());
+            should.not.exist(type.transformValue(null));
+            type.transformValue(1).should.eql('1');
+            var obj = { test: 'test' };
+            type.transformValue(obj).should.eql(JSON.stringify(obj));
+        });
+    });
 });
