2cbcd890149dcd5a70dfb51b14cbfba5f0a8b156
Christopher Cliff <ctcliff@gmail.com>
2013-04-08 19:41:38 -0400

    Added more model tests.

--- test/model.js
+++ test/model.js
@@ -24,15 +24,57 @@ describe('model', function(){
         });
     });
     
+    describe('findOne', function(){
+        
+        
+        
+    });
+    
+    describe('find', function(){
+        
+        
+        
+    });
+    
     describe('create', function(){
         
         it('should create an instance of a model', function(){
             m.should.have.property('attributes');
             m.should.have.property('schema');
+            m.schema.should.equal(schema);
+            m.should.have.property('client');
+            m.client.should.equal(dino.connection.client);
         });
         
+         it('should use the correct client', function(){
+                m.should.have.property('client');
+                m.client.should.equal(dino.connection.client);
+                dino.connect({
+                    accessKeyId: 'AAA',
+                    secretAccessKey: 'AAA',
+                    region: 'us-east-1'
+                });
+                m = model.create({
+                    name: 'chris'
+                });
+                m.client.should.eql(dino.connection.client);
+                var client = dino.connection.create({
+                    accessKeyId: 'BBB',
+                    secretAccessKey: 'BBB',
+                    region: 'us-east-1'
+                });
+                model = dino.model({
+                    schema: schema,
+                    client: client
+                });
+                m = model.create({
+                    name: 'chris'
+                });
+                m.client.should.eql(client);
+            });
+        
         it('should set attribute values', function(){
-            m.attributes.name.should.equal('chris');
+            m.attributes.name.should.eql('chris');
         });
         
     });
@@ -58,6 +100,26 @@ describe('model', function(){
             });
             m.get('name').should.equal('chris');
             should.not.exist(m.get('asdf'));
+        });
+        
+    });
+    
+    describe('save', function(){
+        
+        
+        
+    });
+    
+    describe('destroy', function(){
+        
+        
+        
+    });
+    
+    describe('toJSON', function(){
+        
+        it('should return a JSON representation of the attributes', function(){
+            m.toJSON().should.eql(m.attributes);
         });
         
     });
