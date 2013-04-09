17f43601fbae541516ec49c2040e58c6b37cf4e2
Christopher Cliff <ctcliff@gmail.com>
2013-04-09 16:01:14 -0400

    Added schema tests.

--- test/schema.js
+++ test/schema.js
@@ -1,51 +1,129 @@
 var should = require('should'),
+    moment = require('moment'),
     dino = require('../');
 
 describe('schema', function(){
     
-    it('should set the defaults', function(){
-        
-        var s = dino.schema();
-        
-        s.table.should.equal('');
-        s.attributes.should.eql({});
-        s.key.should.eql({ hash: '' });
-        s.keyDelimiter.should.equal('#');
-        
-        s = dino.schema({
-            table: 'aaa',
-            key: {
-                hash: 'a',
-                range: 'b'
-            },
-            keyDelimiter: 'asdf'
-        });
-        
-        s.table.should.equal('aaa');
-        s.key.should.eql({ hash: 'a', range: 'b' });
-        s.keyDelimiter.should.equal('asdf');
-        
-    });
+    var schema,
+        replySchema;
     
-    it('should add the attributes', function(){
-        
-        var s = dino.schema({
+    beforeEach(function(){
+        dino.connect();
+        schema = dino.schema({
+            table: 'forums',
             attributes: {
-                a: dino.types.boolean
+                name: dino.types.string,
+                misc: dino.types.object
+            },
+            key: {
+                hash: 'name'
             }
         });
+        replySchema = dino.schema({
+            table: 'replies',
+            attributes: {
+                forum_name: dino.types.string,
+                subject: dino.types.string,
+                date_created: dino.types.date,
+                id: dino.types.id
+            },
+            key: {
+                hash: ['forum_name', 'subject'],
+                range: ['date_created', 'id']
+            }
+        });
+    });
+    
+    describe('constructor', function(){
         
-        dino.types.boolean.isPrototypeOf(s.attributes.a).should.be.true;
+        it('should set the defaults', function(){
+            schema.should.have.property('table', 'forums');
+            schema.should.have.property('attributes');
+            schema.should.have.property('hashKeyAttributes');
+            schema.hashKeyAttributes.should.eql(['name']);
+            schema.should.have.property('hashKey', 'name');
+            schema.should.have.property('rangeKey', null);
+            schema.should.have.property('keyDelimiter', '#');
+        });
+        
+        it('should add the attributes', function(){
+            dino.types.string.isPrototypeOf(schema.attributes.name).should.be.true;
+            dino.types.object.isPrototypeOf(schema.attributes.misc).should.be.true;
+        });
         
     });
     
-    it('should create a table', function(){
+    describe('createTable', function(){
         
-        var s = dino.schema();
+        it('should create a table', function(){
+            
+            
+            
+        });
         
-        s.createTable.should.be.a.function;
+    });
+    
+    describe('add', function(){
         
-        s.createTable();
+        it('should add an attribute', function(){
+            
+            schema.add('newAttr', dino.types.boolean);
+            dino.types.boolean.isPrototypeOf(schema.get('newAttr')).should.be.true;
+        });
+        
+    });
+    
+    describe('get', function(){
+        
+        it('should get an attribute', function(){
+            dino.types.string.isPrototypeOf(schema.get('name')).should.be.true;
+            dino.types.object.isPrototypeOf(schema.get('misc')).should.be.true;
+        });
+        
+    });
+    
+    describe('serializeHashAttribute', function(){
+        
+        it('should serialize the hash attribute', function(){
+            schema.serializeHashAttribute('a').should.eql({ S: 'a' });
+            replySchema.serializeHashAttribute(['a', 'b']).should.eql({ S: 'a#b' });
+        });
+        
+    });
+    
+    describe('serializeRangeAttribute', function(){
+        
+        it('should serialize the range attribute', function(){
+            var m = moment.utc(),
+                id = '12345';
+            replySchema.serializeRangeAttribute([m, id]).should.eql({ S: m.format() + '#' + id });
+        });
+        
+    });
+    
+    describe('deserializeHashAttribute', function(){
+        
+        it('should deserialize the hash attribute', function(){
+            var obj = schema.deserializeHashAttribute({ S: 'sometablename' });
+            obj.name.should.equal('sometablename');
+        });
+        
+        it('should deserialize combined hash attributes', function(){
+            var obj = replySchema.deserializeHashAttribute({ S: 'what#ever' });
+            obj.forum_name.should.equal('what');
+            obj.subject.should.equal('ever');
+        });
+        
+    });
+    
+    describe('deserializeRangeAttribute', function(){
+        
+        it('should deserialize the range attribute', function(){
+            var obj = replySchema.deserializeRangeAttribute({ S: '2013-03-27T19:21:54+00:00#12345' });
+            obj.id.should.equal('12345');
+            moment.isMoment(obj.date_created).should.be.true;
+            obj.date_created.format().should.equal('2013-03-27T19:21:54+00:00');
+        });
         
     });
     
