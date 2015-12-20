87dd19a4d4b1aae6a50de81f4be14b88f7cfabfc
Sebastian Wahn <sebastian.wahn@cubesoft.org>
2015-12-20 20:38:31 +0100

    remove and ignore gulpfile.js

--- gulpfile.js
+++
@@ -1,8 +0,0 @@
-var gulp = require('gulp');
-var tsc = require('gulp-tsc');
-gulp.task('build', function () {
-    gulp.src('src/**/*.ts')
-        .pipe(tsc())
-        .pipe(gulp.dest('lib/'));
-});
-//# sourceMappingURL=gulpfile.js.map
