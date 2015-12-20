d793e8bc153da4517d50fab7e85cefe3cf3b8546
Sebastian Wahn <sebastian.wahn@cubesoft.org>
2015-12-20 20:56:19 +0100

    add gulpfile.js again

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+/// <reference path="typings/tsd.d.ts"/>
+var gulp = require('gulp');
+var tsc = require('gulp-tsc');
+gulp.task('build', function () {
+    gulp.src('src/**/*.ts')
+        .pipe(tsc())
+        .pipe(gulp.dest('lib/'));
+});
+//# sourceMappingURL=gulpfile.js.map
